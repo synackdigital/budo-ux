70e33683b654af6903031de946590692487f409e
Fredrik Broman <frebro@gmail.com>
2015-07-14 14:02:04 +0200

    postcss up and running

---
+++ gulpfile.js/tasks/build.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var gulpSequence  = require('gulp-sequence');
+var config = require('../config');
+
+gulp.task('build', ['clean'], function(cb) {
+  gulpSequence('styles', cb);
+});
---
+++ gulpfile.js/tasks/styles.js
@@ -0,0 +1,13 @@
+var config        = require('../config');
+var gulp          = require('gulp');
+var postcss       = require('gulp-postcss');
+var plugins       = require('postcss-load-plugins')();
+var cssnext       = require("cssnext");
+
+gulp.task('styles', function () {
+  return gulp.src(config.app + '/styles/*.css')
+    .pipe(postcss([
+      plugins.cssnext()
+    ]))
+    .pipe(gulp.dest(config.build + '/styles'));
+});
