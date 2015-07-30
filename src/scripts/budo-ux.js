3ebefacd8a5db7be24d65944b22e4629600ef277
Fredrik Broman <frebro@gmail.com>
2015-07-30 17:20:05 +0200

    hidari/migi height equalized with js

--- src/scripts/budo-ux.js
+++ src/scripts/budo-ux.js
@@ -1,11 +1,40 @@
 $(function() {
-
-  /* Enable $.Horunge */
-  $('h1, h2, h3, h4, h5, h6, p', '.article').horunge();
-
-  /* Enable .nav__toggle */
-  $('.nav__toggle').on('click', function(e){
-    $('.nav').toggleClass('nav--expanded');
-    $('body').toggleClass('--no-scroll');
-  });
+  budoUx.init();
 });
+
+var budoUx = {
+
+  init: function() {
+
+    /* Store references to DOM elements */
+    budoUx.$gridHidari = $('.grid__hidari');
+    budoUx.$gridMigi = $('.grid__migi');
+
+
+    /* Enable $.Horunge */
+    $('h1, h2, h3, h4, h5, h6, p', '.article').horunge();
+
+    /* Enable .nav__toggle */
+    $('.nav__toggle').on('click', function(e){
+      $('.nav').toggleClass('nav--expanded');
+      $('body').toggleClass('--no-scroll');
+    });
+
+    /* Set up window listener */
+    $(window).on('resize', budoUx.resize());
+  },
+
+  resize: function() {
+    var hidariHeight = budoUx.$gridHidari.outerHeight();
+    var migiHeight = budoUx.$gridMigi.outerHeight();
+
+    if ( hidariHeight > migiHeight ) {
+      budoUx.$gridHidari.css('height', 'auto');
+      budoUx.$gridMigi.css('height', hidariHeight);
+    }
+    else {
+      budoUx.$gridHidari.css('height', migiHeight);
+      budoUx.$gridMigi.css('height', 'auto');
+    }
+  }
+};
