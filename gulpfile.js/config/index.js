19fb738dc5b3248deb5584c935d45d0b80a70e91
Fredrik Broman <frebro@gmail.com>
2015-08-16 21:03:29 +0200

    renamed dest -> dist

--- gulpfile.js/config/index.js
+++ gulpfile.js/config/index.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.base   = '.';
 config.src    = config.base + '/src';
-config.dest   = config.base + '/dest';
+config.dest   = config.base + '/dist';
 config.bower  = config.base + '/bower_components';
 config.docs   = config.base + '/docs';
 
