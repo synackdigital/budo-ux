dc36de4bc4b52055b43d469d36ca5f3ebf1be1ac
Fredrik Broman <frebro@gmail.com>
2015-08-16 21:27:09 +0200

    made jquery + plugs deps instead of devdeps

--- gulpfile.js/config/scripts.js
+++ gulpfile.js/config/scripts.js
@@ -1,15 +1,11 @@
-var config = require('./')
+var config = require('./');
 
 module.exports = {
   watch: [
-    config.bower + '/jquery/dist/jquery.js',
-    config.bower + '/jquery-horunge/dist/jquery.horunge.js',
     config.src + '/scripts/**/*.js'
   ],
   src: [
-    config.bower + '/jquery/dist/jquery.js',
-    config.bower + '/jquery-horunge/dist/jquery.horunge.js',
     config.src + '/scripts/*.js'
   ],
   dest: config.dest + '/scripts'
-}
+};
