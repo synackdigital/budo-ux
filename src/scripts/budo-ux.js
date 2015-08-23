9b225175c91316807586729e89ce4a77e4de7639
Fredrik Broman <frebro@gmail.com>
2015-08-23 20:10:08 +0200

    nav toggle

--- src/scripts/budo-ux.js
+++ src/scripts/budo-ux.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
       $('h1, h2, h3, h4, h5, h6, p', '.article').horunge();
     }
   }
+
+  /* Enable .nav__toggle */
+  $('.nav__toggle').on('click', function(e){
+    $('.nav').toggleClass('nav--expanded');
+    $('body').toggleClass('no-scroll');
+  });
+
 });
 
 $(window).load(function() {
